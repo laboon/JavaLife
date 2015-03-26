02dacfa0b10a4b7fbb38df717086b0556dd13c3d
Barry <bma30@pitt.edu>
2015-03-26 13:33:02 -0400

    Removed Prints from Tests

--- com/laboon/WorldTest.java
+++ com/laboon/WorldTest.java
@@ -10,35 +10,36 @@ public class WorldTest {
 	@Test
 	public void testsmallgrid() {
 		World w = new World(4, 4, 4);
-		//System.out.println(w.toString());
 		assertEquals(w.toString(),"  0123\n0 ..X.\n1 ....\n2 ....\n3 ....\n");	
 	}
 	
 	@Test
 	public void testallzeros() {
 		World w = new World(0, 0, 0);
-		//System.out.println(w.toString());
 	    assertEquals(w.toString(),"  \n");	
 	}
 	
 	@Test
 	public void testamidgrid() {
 		World w = new World(10, 20, 20);
-		//System.out.println(w.toString());
 	    assertEquals(w.toString(),"  0123456789\n0 ..X.X.....\n1 ..........\n2 ........XX\n3 ..X.....X.\n4 XX.XX...X.\n5 XX..X.X.X.\n6 ....X.....\n7 XX...XX...\n8 ...X...X..\n9 ..........\n");
 	}
 
 	@Test
 	public void testallfull() {
 		World w = new World(4, 0, 100);
-		System.out.println(w.toString());
 		assertEquals(w.toString(),"  0123\n0 XXXX\n1 XXXX\n2 XXXX\n3 XXXX\n");	
+	}
+	
+	@Test
+	public void testtest() {
+		World w = new World(1, 0, 100);
+		assertEquals(w.toString(),"  0\n0 X\n");	
 	}
 	
 	@Test
 	public void testallempty() {
 		World w = new World(4, 0, 0);
-		System.out.println(w.toString());
 		assertEquals(w.toString(),"  0123\n0 ....\n1 ....\n2 ....\n3 ....\n");	
 	}
 
