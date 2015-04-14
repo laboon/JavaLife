16c5207c08990b835d83fa1b1cf5c4edf30f17a7
Jiayan Zhang <jiz86@pitt.edu>
2015-04-14 10:00:34 -0400

    add more unit tests

--- CellUT.java
+++ CellUT.java
@@ -1,13 +1,208 @@
+import org.junit.*;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
-
-
 public class CellUT {
-
+			
+	/////////////////
+	// STATE TESTS //
+	/////////////////
+	
+	// Tests that state() will return ALIVE 
+	// if the current state of the cell is ALIVE
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStateAlive() {
+		Cell cell = new Cell(State.ALIVE, 0, 0);
+		assertEquals(State.ALIVE, cell.state());
 	}
-
+	// Tests that state() will return DEAD 
+	// if the current state of the cell is DEAD
+	@Test
+	public void testStateDead() {
+		Cell cell = new Cell(State.DEAD, 100, 1);
+		assertEquals(State.DEAD, cell.state());
+	}
+	
+	
+	///////////////////
+	// ISALIVE TESTS //
+	///////////////////	
+	
+	// Tests that isAlive() will return true 
+	// if the current state of the cell is ALIVE
+	@Test
+	public void testIsAlive_Alive() {
+		Cell cell = new Cell(State.ALIVE, 1, 99);
+		assertTrue(cell.isAlive());
+	}
+	
+	// Tests that isAlive() will return false 
+	// if the current state of the cell is DEAD
+	@Test
+	public void testIsAlive_Dead() {
+		Cell cell = new Cell(State.DEAD, 0, 199);
+		assertFalse(cell.isAlive());
+	}
+	
+	
+	///////////////////
+	// ITERATE TESTS //
+	///////////////////	
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return DEAD if numNeighbors is 0 and 
+	// the current state of the cell is ALIVE
+	@Test
+	public void testIterate_Alive_0_nb() {
+		Cell cell = new Cell(State.ALIVE, 5, 5);
+		assertEquals(State.DEAD, cell.iterate(0));		
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return DEAD if numNeighbors is 1 and 
+	// the current state of the cell is ALIVE
+	@Test
+	public void testIterate_Alive_1_nb() {
+		Cell cell = new Cell(State.ALIVE, 5, 5);
+		assertEquals(State.DEAD, cell.iterate(1));		
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return ALIVE if numNeighbors is 2 and 
+	// the current state of the cell is ALIVE
+	@Test
+	public void testIterate_Alive_2_nb() {
+		Cell cell = new Cell(State.ALIVE, 5, 5);
+		assertEquals(State.ALIVE, cell.iterate(2));		
+	}
+		
+	// Tests that iterate(int numNeighbors) will 
+	// return ALIVE if numNeighbors is 3 and 
+	// the current state of the cell is ALIVE
+	@Test
+	public void testIterate_Alive_3_nb() {
+		Cell cell = new Cell(State.ALIVE, 5, 5);
+		assertEquals(State.ALIVE, cell.iterate(3));		
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return DEAD if numNeighbors is 4 and 
+	// the current state of the cell is ALIVE
+	@Test
+	public void testIterate_Alive_4_nb() {
+		Cell cell = new Cell(State.ALIVE, 5, 5);
+		assertEquals(State.DEAD, cell.iterate(4));		
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return null if numNeighbors is -8 (invalid) 
+	// and the current state of the cell is ALIVE
+	@Test
+	public void testIterate_Alive_n8_nb() {
+		Cell cell = new Cell(State.ALIVE, 5, 5);
+		assertEquals(null, cell.iterate(-8));		
+	}
+		
+	// Tests that iterate(int numNeighbors) will 
+	// return null if numNeighbors is 20 (invalid) 
+	// and the current state of the cell is ALIVE
+	@Test
+	public void testIterate_Alive_20_nb() {
+		Cell cell = new Cell(State.ALIVE, 5, 5);
+		assertEquals(null, cell.iterate(20));	
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return DEAD if numNeighbors is 0 and 
+	// the current state of the cell is DEAD
+	@Test
+	public void testIterate_Dead_0_nb() {
+		Cell cell = new Cell(State.DEAD, 5, 5);
+		assertEquals(State.DEAD, cell.iterate(0));		
+	}
+		
+	// Tests that iterate(int numNeighbors) will 
+	// return DEAD if numNeighbors is 2 and 
+	// the current state of the cell is DEAD
+	@Test
+	public void testIterate_Dead_2_nb() {
+		Cell cell = new Cell(State.DEAD, 5, 5);
+		assertEquals(State.DEAD, cell.iterate(2));		
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return ALIVE if numNeighbors is 3 and 
+	// the current state of the cell is DEAD
+	@Test
+	public void testIterate_Dead_3_nb() {
+		Cell cell = new Cell(State.DEAD, 5, 5);
+		assertEquals(State.ALIVE, cell.iterate(3));		
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return DEAD if numNeighbors is 4 and 
+	// the current state of the cell is DEAD
+	@Test
+	public void testIterate_Dead_4_nb() {
+		Cell cell = new Cell(State.DEAD, 5, 5);
+		assertEquals(State.DEAD, cell.iterate(4));		
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return null if numNeighbors is -15 (invalid) 
+	// and the current state of the cell is DEAD
+	@Test
+	public void testIterate_Dead_n15_nb() {
+		Cell cell = new Cell(State.DEAD, 5, 5);
+		assertEquals(null, cell.iterate(-15));		
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return null if numNeighbors is 15 (invalid) 
+	// and the current state of the cell is DEAD
+	@Test
+	public void testIterate_Dead_15_nb() {
+		Cell cell = new Cell(State.DEAD, 5, 5);
+		assertEquals(null, cell.iterate(15));		
+	}
+	
+	// Tests that iterate(int numNeighbors) will 
+	// return null if numNeighbors is 5 and
+	// the current state of the cell is null
+	@Test
+	public void testIterate_null_5_nb() {
+		Cell cell = new Cell(null, 5, 5);
+		assertEquals(null, cell.iterate(5));		
+	}
+	
+	
+	///////////////////////
+	// GETSTATEREP TESTS //
+	///////////////////////
+	
+	// Tests that getStateRep() will return
+	// the character '.' if the current state
+	// of the cell is DEAD
+	@Test
+	public void testGetStateRep_Alive() {
+		Cell cell = new Cell(State.DEAD, 5, 5);
+		assertEquals('.', cell.getStateRep());		
+	}
+	
+	// Tests that getStateRep() will return
+	// the character 'X' if the current state
+	// of the cell is ALIVE
+	@Test
+	public void testGetStateRep_Dead() {
+		Cell cell = new Cell(State.DEAD, 5, 5);
+		assertEquals('.', cell.getStateRep());	
+	}
+	
+	// Tests that getStateRep() will return
+	// the character '?' if the current state
+	// of the cell is null
+	@Test
+	public void testGetStateRep_Null() {
+		Cell cell = new Cell(null, 5, 5);
+		assertEquals('?', cell.getStateRep());		
+	}	
 }
--- WorldUT.java
+++ WorldUT.java
@@ -1,15 +1,9 @@
+import org.junit.*;
+import java.util.Random;
 import static org.junit.Assert.*;
-
-import org.junit.Test;
-
+import static org.mockito.Mockito.*;
 
 public class WorldUT {
-
-	////////////////////
-	// ITERATE TESTS //
-	////////////////////
-	
-	
 	
 	////////////////////
 	// TOSTRING TESTS //
@@ -21,7 +15,17 @@ public class WorldUT {
 	public void test_interior_value() 
 	{		
 		World myWorld = new World(10, 5, 30);
-		String expectedValue = "  0123456789\n0 ...XXX..XX\n1 .X..X..XXX\n2 X.X..X..XX\n3 ...XX.....\n4 ..........\n5 XXX.XX.X..\n6 ..XX.....X\n7 ........X.\n8 .......X..\n9 .XXXX.X...\n";
+		String expectedValue = "  0123456789\n" + 
+							   "0 ...XXX..XX\n" + 
+							   "1 .X..X..XXX\n" + 
+							   "2 X.X..X..XX\n" + 
+							   "3 ...XX.....\n" +
+							   "4 ..........\n" +
+							   "5 XXX.XX.X..\n" +
+							   "6 ..XX.....X\n" +
+							   "7 ........X.\n" +
+							   "8 .......X..\n" +
+							   "9 .XXXX.X...\n";
 		assertEquals(expectedValue, myWorld.toString());		
 	}
 	
@@ -31,8 +35,9 @@ public class WorldUT {
 	public void test_edge_size() 
 	{
 		World myWorld = new World(1, 7, 20);
-		String expectedValue = "  0\n0 .\n";
-		assertEquals(expectedValue, myWorld.toString());	
+		String expectedValue = "  0\n" + 
+							   "0 .\n";
+		assertEquals(expectedValue, myWorld.toString());
 	}
 	
 	// Tests that in a 6*6 world of 0 percent aliveness, 
@@ -41,7 +46,13 @@ public class WorldUT {
 	public void test_zero_percent() 
 	{
 		World myWorld = new World(6, 9, 0);
-		String expectedValue = "  012345\n0 ......\n1 ......\n2 ......\n3 ......\n4 ......\n5 ......\n";
+		String expectedValue = "  012345\n" + 
+							   "0 ......\n" +
+							   "1 ......\n" +
+							   "2 ......\n" +
+							   "3 ......\n" +
+							   "4 ......\n" +
+							   "5 ......\n";
 		assertEquals(expectedValue, myWorld.toString());		
 	}
 	
@@ -51,7 +62,76 @@ public class WorldUT {
 	public void test_full_percent() 
 	{
 		World myWorld = new World(8, 11, 100);
-		String expectedValue = "  01234567\n0 XXXXXXXX\n1 XXXXXXXX\n2 XXXXXXXX\n3 XXXXXXXX\n4 XXXXXXXX\n5 XXXXXXXX\n6 XXXXXXXX\n7 XXXXXXXX\n";		
+		String expectedValue = "  01234567\n" + 
+							   "0 XXXXXXXX\n" + 
+							   "1 XXXXXXXX\n" + 
+							   "2 XXXXXXXX\n" + 
+							   "3 XXXXXXXX\n" + 
+							   "4 XXXXXXXX\n" + 
+							   "5 XXXXXXXX\n" + 
+							   "6 XXXXXXXX\n" + 
+							   "7 XXXXXXXX\n";		
 		assertEquals(expectedValue, myWorld.toString());		
+	}
+	
+	// Tests that in a 0*0 world of 10 percent aliveness, 
+	// an empty string will be printed out
+	@Test
+	public void test_world_size_zero() 
+	{
+		World myWorld = new World(0, 19, 10);
+		String expectedValue = "  \n";
+		assertEquals(expectedValue, myWorld.toString());		
+	}
+	
+	// Tests that passing in a negative number (invalid) 
+	// for the aliveness percentage  will result in an 
+	// invalid World and so print out a null string
+	@Test
+	public void test_world_negative_percent() 
+	{
+		World myWorld = new World(0, 4, -17);
+		String expectedValue = null;
+		assertEquals(expectedValue, myWorld.toString());	
+	}
+	
+	
+	///////////////////
+	// ITERATE TESTS //
+	///////////////////
+	
+	// Tests that after iterating a 5*5 world of 30 percent 
+	// aliveness, the new world coming out matches the expected world
+	@Test
+	public void test_iterate_1() 
+	{				
+		World myWorld = new World(10, 5, 30);	
+		String expectedWorld = "  0123456789\n" + 
+							   "0 .X....X..X\n" + 
+							   "1 .XX...XX..\n" + 
+							   "2 XXX..X.X..\n" + 
+							   "3 ...XX....X\n" + 
+							   "4 .XX..X....\n" + 
+							   "5 XXX.X.....\n" + 
+							   "6 X.XXX...XX\n" + 
+							   "7 ........X.\n" + 
+							   "8 ..XX...X..\n" + 
+							   "9 ..X...XXX.\n";
+		assertEquals(expectedWorld, myWorld.iterate().toString());	
+//		Cell room = mock(Cell.class);
+//		Cell[][] newCells = new Cell[3][3];
+//		newCells[0][0] = new Cell(State.DEAD, 0,0); 
+//		newCells[0][1] = new Cell(State.DEAD, 0,1); 
+//		newCells[0][2] = new Cell(State.DEAD, 0,2); 
+//		newCells[1][0] = new Cell(State.ALIVE, 1,0); 
+//		newCells[1][1] = new Cell(State.ALIVE, 1,1); 
+//		newCells[1][2] = new Cell(State.ALIVE, 1,2); 
+//		newCells[2][0] = new Cell(State.DEAD,2,0); 
+//		newCells[2][1] = new Cell(State.DEAD, 2,1); 
+//		newCells[2][2] = new Cell(State.ALIVE, 2,2); 
+//		Random rnd = new Random(5);
+//		World expectedWorld = new World(newCells, rnd);	
+//		World myWorld = new World(3, 5, 30);
+//		assertEquals(expectedWorld.iterate(), myWorld.iterate());
 	}
 }
